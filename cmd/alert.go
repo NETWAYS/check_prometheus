d1d217151b16210dc471b0eaa1f5cf8089dd1628
Philipp Dorschner <philipp.dorschner@netways.de>
2022-09-29 15:58:19 +0200

    WIP

--- cmd/alert.go
+++ cmd/alert.go
@@ -1,23 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/NETWAYS/check_prometheus/internal/alert"
 	"github.com/NETWAYS/go-check"
+	"github.com/NETWAYS/go-check/result"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/spf13/cobra"
 )
-
-const (
-	// Possible values for Alert States.
-	StateFiring   = "firing"
-	StateInactive = "inactive"
-	StatePending  = "pending"
-)
-
-type AlertConfig struct {
-	Name string
-}
-
-var cliAlertConfig AlertConfig
 
 var alertCmd = &cobra.Command{
 	Use:   "alert",
@@ -31,9 +21,20 @@ The alert status is:
 	pending = WARNING
 	firing = CRITICAL`,
 	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			states          []int
+			output          string
+			summary         string
+			counterAlert    int
+			counterFiring   int
+			counterPending  int
+			counterInactive int
+		)
+
+		rule := alert.Alertingrule{}
+
 		c := cliConfig.Client()
 		err := c.Connect()
-
 		if err != nil {
 			check.ExitError(err)
 		}
@@ -42,37 +43,154 @@ The alert status is:
 		defer cancel()
 		// We use the Rules endpoint since it contains
 		// the state of inactive Alert Rules, unlike the Alert endpoint
-		result, err := c.Api.Rules(ctx)
-
+		// Search requested Alert in all Groups and all Rules
+		alerts, err := c.Api.Rules(ctx)
 		if err != nil {
 			check.ExitError(err)
 		}
 
-		// Search requested Alert in all Groups and all Rules
-		for _, grp := range result.Groups {
+		summary += fmt.Sprintf("Found ")
+
+		// TODO SUPER SUPER hacky, needs to be refactored ASAP!
+		// Search for specified groups
+		for _, grp := range alerts.Groups {
 			for _, rl := range grp.Rules {
-				rule, ok := rl.(v1.AlertingRule)
-				if ok && rule.Name == cliAlertConfig.Name {
-					switch rule.State {
-					case StateInactive:
-						check.Exitf(check.OK, "Alert %s inactive", cliAlertConfig.Name)
-					case StatePending:
-						check.Exitf(check.Warning, "Alert %s pending", cliAlertConfig.Name)
-					case StateFiring:
-						check.Exitf(check.Critical, "Alert %s firing", cliAlertConfig.Name)
+				rule.Alertingrule = rl.(v1.AlertingRule)
+				// Search for specified alert names
+				if cliAlertConfig.AlertName == nil {
+					if len(rule.Alertingrule.Alerts) == 0 {
+						counterAlert++
+
+						switch rule.GetStatus() {
+						case 0:
+							counterInactive++
+						case 1:
+							counterPending++
+						case 2:
+							counterFiring++
+						}
+
+						states = append(states, rule.GetStatus())
+
+						if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+							output += " \\_"
+						}
+
+						output += fmt.Sprintf("[%s] %s", check.StatusText(rule.GetStatus()), rule.GetOutput())
+
+						if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+							output += " \n"
+						}
+					} else {
+						for _, alert := range rule.Alertingrule.Alerts {
+							counterAlert++
+
+							switch rule.GetStatus() {
+							case 0:
+								counterInactive++
+							case 1:
+								counterPending++
+							case 2:
+								counterFiring++
+							}
+
+							rule.Alert = alert
+							states = append(states, rule.GetStatus())
+
+							if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+								output += " \\_"
+							}
+
+							output += fmt.Sprintf("[%s] %s", check.StatusText(rule.GetStatus()), rule.GetOutput())
+
+							if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+								output += " \n"
+							}
+						}
+					}
+				} else {
+					for _, name := range cliAlertConfig.AlertName {
+						if name == rule.Alertingrule.Name {
+							if len(rule.Alertingrule.Alerts) == 0 {
+								counterAlert++
+
+								switch rule.GetStatus() {
+								case 0:
+									counterInactive++
+								case 1:
+									counterPending++
+								case 2:
+									counterFiring++
+								}
+
+								states = append(states, rule.GetStatus())
+
+								if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+									output += " \\_"
+								}
+
+								output += fmt.Sprintf("[%s] %s", check.StatusText(rule.GetStatus()), rule.GetOutput())
+
+								if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+									output += " \n"
+								}
+							} else {
+								for _, alert := range rule.Alertingrule.Alerts {
+									counterAlert++
+
+									switch rule.GetStatus() {
+									case 0:
+										counterInactive++
+									case 1:
+										counterPending++
+									case 2:
+										counterFiring++
+									}
+
+									rule.Alert = alert
+									states = append(states, rule.GetStatus())
+
+									if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+										output += " \\_"
+									}
+
+									output += fmt.Sprintf("[%s] %s", check.StatusText(rule.GetStatus()), rule.GetOutput())
+
+									if len(cliAlertConfig.AlertName) > 1 || cliAlertConfig.Group == nil {
+										output += " \n"
+									}
+								}
+							}
+						}
 					}
 				}
 			}
 		}
-		check.Exitf(check.Unknown, "Alert %s not found", cliAlertConfig.Name)
+
+		summary += fmt.Sprintf("%d alerts - firing %d - pending %d - inactive %d",
+			counterAlert,
+			counterFiring,
+			counterPending,
+			counterInactive)
+
+		if result.WorstState(states...) == 0 {
+			check.ExitRaw(result.WorstState(states...), "All alerts are inactive")
+		} else {
+			check.ExitRaw(result.WorstState(states...), summary+"\n"+output)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(alertCmd)
 	fs := alertCmd.Flags()
-	fs.StringVarP(&cliAlertConfig.Name, "name", "n", "",
-		"The name of the alert to check")
-
-	_ = alertCmd.MarkFlagRequired("name")
+	fs.StringSliceVarP(&cliAlertConfig.AlertName, "name", "n", nil,
+		"The name of one or more specific alerts to check."+
+			"\nThis parameter can be repeated e.G.: '--name alert1 --name alert2'"+
+			"\nIf no name is given, all alerts will be evaluated")
+	//fs.StringSliceVarP(&cliAlertConfig.Group, "group", "g", nil,
+	//	"The name of one or more specific groups to check."+
+	//		"\nThis parameter can be repeated e.G.: '--name group1 --name group2'")
+	fs.BoolVarP(&cliAlertConfig.ShowAll, "show-all", "a", false,
+		"Displays all alerts regardless of the status")
 }
