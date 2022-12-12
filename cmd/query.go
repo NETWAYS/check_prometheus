d4daa06eebb91081f8a7b1599c829c329f50446c
Markus Opolka <markus.opolka@netways.de>
2022-12-12 14:14:31 +0100

    Refactor query.go

--- cmd/query.go
+++ cmd/query.go
@@ -1,14 +1,13 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/NETWAYS/go-check"
 	"github.com/NETWAYS/go-check/perfdata"
 	goresult "github.com/NETWAYS/go-check/result"
-	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
 	"github.com/spf13/cobra"
+	"strings"
 	"time"
 )
 
@@ -27,26 +26,39 @@ type User struct {
 
 var cliQueryConfig QueryConfig
 
+func generateMetricOutput(rc int, metric string, value string) string {
+	// Format the metric and RC output for console output
+	return fmt.Sprintf(" \\_[%s] %s - value: %s\n", check.StatusText(rc), metric, value)
+}
+
+func generatePerfdata(metric string, value string) perfdata.Perfdata {
+	// We trim the trailing "} from the string, so that the Perfdata won't have a trailing _
+	return perfdata.Perfdata{
+		Label: strings.ReplaceAll(strings.TrimRight(metric, "\"}"), " ", ""),
+		Value: value,
+	}
+}
+
 var queryCmd = &cobra.Command{
 	Use:   "query",
 	Short: "Checks the status of a Prometheus query",
 	Long: `Checks the status of a Prometheus query and evaluates the result of the alert.
 Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest value will be evaluated, other values will be ignored!`,
-	Example: `  $ check_prometheus query -q 'go_gc_duration_seconds_count' -c 5000 -w 2000   
-  CRITICAL - 2 Metrics: 1 Critical - 0 Warning - 1 Ok
-   \_[OK] go_gc_duration_seconds_count{instance="localhost:9090", job="prometheus"} - value: 1599
-   \_[CRITICAL] go_gc_duration_seconds_count{instance="node-exporter:9100", job="node-exporter"} - value: 79610
-   | value_go_gc_duration_seconds_count_localhost:9090_prometheus=1599 value_go_gc_duration_seconds_count_node-exporter:9100_node-exporter=79610`,
+	Example: `
+	$ check_prometheus query -q 'go_gc_duration_seconds_count' -c 5000 -w 2000
+	CRITICAL - 2 Metrics: 1 Critical - 0 Warning - 1 Ok
+	 \_[OK] go_gc_duration_seconds_count{instance="localhost:9090", job="prometheus"} - value: 1599
+	 \_[CRITICAL] go_gc_duration_seconds_count{instance="node-exporter:9100", job="node-exporter"} - value: 79610
+	 | value_go_gc_duration_seconds_count_localhost:9090_prometheus=1599 value_go_gc_duration_seconds_count_node-exporter:9100_node-exporter=79610`,
 	PreRun: func(cmd *cobra.Command, args []string) {
 		if cliQueryConfig.Warning == "" || cliQueryConfig.Critical == "" {
-			check.ExitError(fmt.Errorf("please specify warning and critical thresholds"))
+			check.ExitError(fmt.Errorf("Please specify warning and critical thresholds"))
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
 			rc             int
 			states         []int
-			output         string
 			metricOutput   string
 			summary        string
 			metricsCounter int
@@ -75,8 +87,6 @@ Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest
 		ctx, cancel := cliConfig.timeoutContext()
 		defer cancel()
 
-		// TODO Should time be a flag?
-
 		result, warnings, err := c.Api.Query(ctx, cliQueryConfig.RawQuery, time.Now())
 		if err != nil {
 			check.ExitError(err)
@@ -84,19 +94,26 @@ Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest
 
 		//valType := query.ValType{}
 		switch result.Type() {
+		// model.ValNone
+		default:
+			check.ExitError(fmt.Errorf("None value results are not supported"))
 		// Scalar - a simple numeric floating point value
 		case model.ValScalar:
-			check.ExitError(fmt.Errorf("scalar value results are not supported"))
+			check.ExitError(fmt.Errorf("Scalar value results are not supported"))
+		case model.ValString:
+			// String - a simple string value; currently unused
+			check.ExitError(fmt.Errorf("String value results are not supported"))
 		case model.ValVector:
 			// Instant vector - a set of time series containing a single sample for each time series, all sharing the same timestamp
 			vectorVal := result.(model.Vector)
 
-			for _, metric := range vectorVal {
+			for _, sample := range vectorVal {
 				metricsCounter++
-				if crit.DoesViolate(float64(metric.Value)) {
+
+				if crit.DoesViolate(float64(sample.Value)) {
 					rc = check.Critical
 					critCounter++
-				} else if warn.DoesViolate(float64(metric.Value)) {
+				} else if warn.DoesViolate(float64(sample.Value)) {
 					rc = check.Warning
 					warnCounter++
 				} else {
@@ -105,49 +122,11 @@ Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest
 				}
 
 				states = append(states, rc)
+				// Format the metric and RC output for console output
+				metricOutput += generateMetricOutput(rc, sample.Metric.String(), sample.Value.String())
 
-				output = fmt.Sprintf(" \\_[%s] %s - value: %s\n", check.StatusText(rc), metric.Metric.String(), metric.Value)
-
-				metricOutput += output
-				output = ""
-
-				// Marshalls the Prometheus output -> go_gc_duration_seconds_count{instance="node-exporter:9100", job="node-exporter"}
-				// Need the JSON to fill the performance data with the labels
-				jsn, err := metric.MarshalJSON()
-				if err != nil {
-					check.ExitError(err)
-				}
-
-				var res map[string]interface{}
-
-				err = json.Unmarshal(jsn, &res)
-				if err != nil {
-					check.ExitError(err)
-				}
-
-				// map[metric:map[__name__:go_goroutines instance:localhost:9090 job:prometheus] value:[1.669290557403e+09 38]]
-				mtr := res["metric"].(map[string]interface{})
-				var (
-					metrName string
-					instance string
-					job      string
-				)
-				for key, val := range mtr {
-					switch key {
-					case "__name__":
-						metrName = fmt.Sprintf("%v", val)
-					case "instance":
-						instance = fmt.Sprintf("%v", val)
-					case "job":
-						job = fmt.Sprintf("%v", val)
-					}
-				}
-
-				perf := perfdata.Perfdata{
-					Label: fmt.Sprintf("value_%s_%s_%s", metrName, instance, job),
-					Value: metric.Value,
-				}
-
+				// Generate Perfdata from API return
+				perf := generatePerfdata(sample.Metric.String(), sample.Value.String())
 				perfList.Add(&perf)
 			}
 		case model.ValMatrix:
@@ -157,14 +136,14 @@ Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest
 			// Note: Only the latest value will be evaluated, other values will be ignored!
 			matrixVal := result.(model.Matrix)
 
-			for _, val := range matrixVal {
+			for _, samplestream := range matrixVal {
 				metricsCounter++
-				v := val.Values[len(val.Values)-1]
+				samplepair := samplestream.Values[len(samplestream.Values)-1]
 
-				if crit.DoesViolate(float64(v.Value)) {
+				if crit.DoesViolate(float64(samplepair.Value)) {
 					rc = check.Critical
 					critCounter++
-				} else if warn.DoesViolate(float64(v.Value)) {
+				} else if warn.DoesViolate(float64(samplepair.Value)) {
 					rc = check.Warning
 					warnCounter++
 				} else {
@@ -173,54 +152,31 @@ Note: Time range values e.G. 'go_memstats_alloc_bytes_total[0s]' only the latest
 				}
 
 				states = append(states, rc)
+				// Format the metric and RC output for console output
+				metricOutput += generateMetricOutput(rc, samplepair.String(), samplepair.Value.String())
 
-				output = fmt.Sprintf(" \\_[%s] %s - value: %s\n", check.StatusText(rc), val.Metric.String(), v.Value)
-
-				metricOutput += output
-				output = ""
-
-				var (
-					metrName string
-					instance string
-					job      string
-				)
-				for key, value := range val.Metric {
-					switch key {
-					case "__name__":
-						metrName = fmt.Sprintf("%v", value)
-					case "instance":
-						instance = fmt.Sprintf("%v", value)
-					case "job":
-						job = fmt.Sprintf("%v", value)
-					}
-				}
-
-				perf := perfdata.Perfdata{
-					Label: fmt.Sprintf("value_%s_%s_%s", metrName, instance, job),
-					Value: v.Value,
-				}
-
+				// Generate Perfdata from API return
+				perf := generatePerfdata(samplestream.Metric.String(), samplepair.Value.String())
 				perfList.Add(&perf)
 			}
-		case model.ValString:
-			// String - a simple string value; currently unused
-			check.ExitError(fmt.Errorf("string value results are not supported"))
-		default:
-			// model.ValNone
-			check.ExitError(fmt.Errorf("none value results are not supported"))
 		}
 
+		// The worst state of all metrics determines the final return state. Example:
+		// OK, OK, OK > OK
+		// Critical, OK, OK > Critical
 		worstState := goresult.WorstState(states...)
 		if worstState == check.OK {
 			summary += fmt.Sprintf("%d Metrics OK", metricsCounter)
 		} else {
 			summary += fmt.Sprintf("%d Metrics: %d Critical - %d Warning - %d Ok\n", metricsCounter, critCounter, warnCounter, okCounter)
 			summary += metricOutput
-
 		}
 
 		// Should be printed after the Check Plugin output
-		defer printWarning(warnings)
+		// Defer doesn't work because of the os.Exit
+		if len(warnings) > 0 {
+			summary += fmt.Sprintf("HTTP Warnings: %v\n", strings.Join(warnings, ", "))
+		}
 
 		check.ExitRaw(goresult.WorstState(states...), summary, "|", perfList.String())
 	},
@@ -241,11 +197,4 @@ func init() {
 
 	fs.SortFlags = false
 	_ = queryCmd.MarkFlagRequired("query")
-}
-
-func printWarning(warnings v1.Warnings) {
-	// TODO Validate this
-	if len(warnings) > 0 {
-		fmt.Printf("Warnings: %v\n", warnings)
-	}
 }
--- cmd/query_test.go
+++ cmd/query_test.go
@@ -46,6 +46,15 @@ type QueryTest struct {
 func TestQueryCmd(t *testing.T) {
 	tests := []QueryTest{
 		{
+			name: "query-warning",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"status":"success","data":{"resultType":"vector","result":[]},"warnings": ["hic sunt dracones", "foo"]}`))
+			})),
+			args:     []string{"run", "../main.go", "query", "--query", "foo"},
+			expected: "UNKNOWN - 0 Metrics: 0 Critical - 0 Warning - 0 Ok\nHTTP Warnings: hic sunt dracones, foo\n | \nexit status 3\n",
+		},
+		{
 			name: "query-no-such-metric",
 			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
@@ -55,13 +64,40 @@ func TestQueryCmd(t *testing.T) {
 			expected: "UNKNOWN - 0 Metrics: 0 Critical - 0 Warning - 0 Ok\n | \nexit status 3\n",
 		},
 		{
+			name: "query-no-such-matrix",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"status":"success","data":{"resultType":"matrix","result":[]}}`))
+			})),
+			args:     []string{"run", "../main.go", "query", "--query", "foo"},
+			expected: "UNKNOWN - 0 Metrics: 0 Critical - 0 Warning - 0 Ok\n | \nexit status 3\n",
+		},
+		{
+			name: "query-scalar",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"status":"success","data":{"resultType":"scalar","result":[1670339013.992,"1"]}}`))
+			})),
+			args:     []string{"run", "../main.go", "query", "--query", "1"},
+			expected: "UNKNOWN - Scalar value results are not supported (*errors.errorString)\nexit status 3\n",
+		},
+		{
+			name: "query-matrix-exists",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"status":"success","data":{"resultType":"matrix","result":[{"metric":{"__name__":"up","instance":"localhost","job":"node"},"values":[[1670340712.988,"1"],[1670340772.988,"1"],[1670340832.990,"1"],[1670340892.990,"1"],[1670340952.990,"1"]]}]}}`))
+			})),
+			args:     []string{"run", "../main.go", "query", "--query", "up{job=\"prometheus\"}[5m]"},
+			expected: "OK - 1 Metrics OK | up_instance_localhost_job_node=1\n",
+		},
+		{
 			name: "query-metric-exists",
 			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
 				w.Write([]byte(`{"status":"success","data":{"resultType":"vector","result":[{"metric":{"__name__":"up","instance":"localhost","job":"prometheus"},"value":[1668782473.835,"1"]}]}}`))
 			})),
 			args:     []string{"run", "../main.go", "query", "--query", "up{job=\"prometheus\"}"},
-			expected: "OK - 1 Metrics OK | value_up_localhost_prometheus=1\n",
+			expected: "OK - 1 Metrics OK | up_instance_localhost_job_prometheus=1\n",
 		},
 	}
 
